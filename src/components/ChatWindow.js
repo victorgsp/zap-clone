7430a3b8de152fcf099243436f71b465dd9d250c
Victor <you@example.com>
2020-09-15 08:19:18 -0300

    chatwindow implementation

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,8 +1,69 @@
 import React from 'react';
+import EmojiPicker from 'emoji-picker-react';
 import './ChatWindow.css';
 
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
+
+const handleEmojiClick = () => {
+    alert('teste');
+};
 export default () => {
     return (
-       <div>teste</div>
+        <div className="chatWindow">
+            <div className="chatWindow--header">
+                <div className="chatWindow--headerinfo">
+                    <img className="chatWindow--avatar" src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg" alt=""></img>
+                    <div className="chatWindow--name">Bonieky Lacerda</div>
+                </div>
+                <div className="chatWindow--headerbuttons">
+                    <div className="chatWindow--btn">
+                        <SearchIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <AttachFileIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <MoreVertIcon style={{ color: '#919191' }} />
+                    </div>
+                </div>
+            </div>
+            <div className="chatWindow--body">
+
+            </div>
+            <div className="chatWindow--emojiarea">
+                <EmojiPicker 
+                    onEmojiClick={handleEmojiClick}
+                    disableSearchBar
+                    disableSkinTonePicker/>
+            </div>
+            <div className="chatWindow--footer">
+                <div className="chatWindow--pre">
+                    <div className="chatWindow--btn">
+                        <CloseIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <InsertEmoticonIcon style={{ color: '#919191' }} />
+                    </div>
+                </div>
+                <div className="chatWindow--inputarea">
+                    <input 
+                        className="chatWindow--input"
+                        type="text"
+                        placeholder="Digite uma mensagem"/>
+                </div>
+                <div className="chatWindow--pos">
+                    <div className="chatWindow--btn">
+                        <SendIcon   style={{ color: '#919191' }} />
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 }
